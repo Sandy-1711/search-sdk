d16f11fd7525acad885b96a603de430ef9a01b04
Sandy-1711 <sandy1711003@gmail.com>
2026-07-18 16:51:04 +0530

    feat: extend TavilyProvider with Extract and Crawl functionality, enhancing type safety

--- providers/tavily/src/index.ts
+++ providers/tavily/src/index.ts
@@ -1,5 +1,5 @@
 import type { SearchProvider, BaseParams } from "@search-sdk/core";
-import type { TavilyClient, TavilySearchOptions, TavilyClientOptions } from "@tavily/core";
+import type { TavilyClient, TavilySearchOptions, TavilyClientOptions, TavilyExtractOptions, TavilyCrawlOptions } from "@tavily/core";
 import { tavily } from "@tavily/core"
 import type { ExtractProvider } from "@search-sdk/core";
 import type { CrawlProvider } from "@search-sdk/core";
@@ -7,11 +7,20 @@ import type { CrawlProvider } from "@search-sdk/core";
 
 
 export interface TavilySearchParams extends BaseParams, TavilySearchOptions { }
+interface CrawlBaseParam {
+    url: string;
+}
+interface ExtractBaseParam {
+    urls: string[];
+}
+
+type ExtractParams = ExtractBaseParam & TavilyExtractOptions;
+type CrawlParams = CrawlBaseParam & TavilyCrawlOptions;
+
 
 export class TavilyProvider implements SearchProvider<TavilySearchParams, any>,
-    ExtractProvider<TavilyExtractParams, any>,
-    CrawlProvider<TavilyCrawlParams, any>,
-{
+    ExtractProvider<ExtractParams, any>,
+    CrawlProvider<CrawlParams, any> {
 
     #client: TavilyClient;
     constructor(config: TavilyClientOptions) {
@@ -20,6 +29,16 @@ export class TavilyProvider implements SearchProvider<TavilySearchParams, any>,
     async search(params: TavilySearchParams): Promise<any> {
         const { query } = params;
         const response = await this.#client.search(query, params);
+        return response;
+    }
+    async extract(params: ExtractParams): Promise<any> {
+        const { urls } = params;
+        const response = await this.#client.extract(urls, params);
+        return response;
+    }
+    async crawl(params: CrawlParams): Promise<any> {
+        const { url } = params;
+        const response = await this.#client.crawl(url, params);
         return response;
     }
 }
