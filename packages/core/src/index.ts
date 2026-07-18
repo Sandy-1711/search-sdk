77534c3a992e1798ad60243ac137dd205b6f9bb6
Sandy-1711 <sandy1711003@gmail.com>
2026-07-18 15:27:06 +0530

    fix: correct property name in SearchConfig interface for clarity

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,18 +1,32 @@
 import { SearchProvider, type InferParams, type InferResult } from "./provider";
 
 interface SearchConfig<TProvider extends SearchProvider<any, any>> {
-    P: TProvider;
+    provider: TProvider;
 }
 
 export class Search<
     P extends SearchProvider<any, any>,
 > {
+    /**
+     * The SearchProvider instance that will be used to perform the search operations.
+     */
     #TProvider: P;
+
+    /**
+     * Constructor to initialize the private variables of the Search class.
+     * @param config - The configuration object containing the SearchProvider instance.
+     */
     constructor(
         config: SearchConfig<P>
     ) {
-        this.#TProvider = config.P;
+        this.#TProvider = config.provider;
     }
+
+    /**
+     * Invokes the search operation using the configured SearchProvider.
+     * @param params Provider agnostic search params
+     * @returns Provider agnostic search results
+     */
     invoke(params: InferParams<P>): Promise<InferResult<P>> {
         return this.#TProvider.search(params);
     }
